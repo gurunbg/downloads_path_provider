a2ef160f98fc02146f4a2e3c471d2bd3485d5a21
gurunbg <gurun.bg@gmail.com>
2021-09-13 23:18:46 +0700

    implement v2 Android embedding

--- android/src/main/java/it/nplace/downloadspathprovider/DownloadsPathProviderPlugin.java
+++ android/src/main/java/it/nplace/downloadspathprovider/DownloadsPathProviderPlugin.java
@@ -7,18 +7,36 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 
 /**
  * DownloadsPathProviderPlugin
  */
-public class DownloadsPathProviderPlugin implements MethodCallHandler {
+public class DownloadsPathProviderPlugin implements FlutterPlugin, MethodCallHandler {
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        pluginRegisterWith(binding.getBinaryMessenger());
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        // TODO: your plugin is no longer attached to a Flutter experience.
+    }
+
     /**
      * Plugin registration.
      */
     public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "downloads_path_provider_28");
+        pluginRegisterWith(registrar.messenger());
+    }
+
+    private static void pluginRegisterWith(BinaryMessenger messanger) {
+        final MethodChannel channel = new MethodChannel(messanger, "downloads_path_provider_28");
         channel.setMethodCallHandler(new DownloadsPathProviderPlugin());
     }
+
 
     @Override
     public void onMethodCall(MethodCall call, Result result) {
